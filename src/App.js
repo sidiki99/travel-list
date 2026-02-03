8d2cb91be36f6f15e893ed251048bfe07a7383d0
sidiki99 <msidiki075@gmail.com>
2026-02-03 17:44:28 +0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
-function App() {
+export default  function App(){
+  return(
+
+  <div className="app">
+   <Logo />
+   <Form />
+   <PackingList />
+   <Stats />
+  </div>
+  )
+}
+
+function Logo() {
+  return(
+  <h1>🏝️ Far Away 🧳</h1>
+  )
+}
+
+function Form() {
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+  return(
+   <form className="add-form" onSubmit={handleSubmit}>
+    <h3>What do you need for your 😍 trip?</h3>
+    <select>
+      {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+        <option value={num} key={num}>
+          {num}
+        </option>
+      ))}
+    </select>
+    <input type="text" placeholder=" item..." />
+    <button>Add</button>
+
+  </form>)
+}
+
+function PackingList() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+   <ul> 
+        {initialItems.map((item) => (
+          <Item item={item}  />
+        ))}
+    </ul>    
+   
     </div>
   );
 }
 
-export default App;
+function Item({ item }) {
+  return <li> 
+    <span style={item.packed ? {textDecoration:"line-through"} :{}}>
+     {item.quantity} {item.description}
+   
+    </span>
+    <button style={{ color: "red" }}>
+        &times;
+    </button>
+
+      </li>;
+}
+
+function Stats(){
+  return(<footer  className=" stats">
+    <em>
+     🧳 You have X items on your list , and You have already packed X(x%) </em>
+  </footer>
+  )
+
+}
