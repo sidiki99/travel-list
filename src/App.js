08244298d03d0b2da203877973e4f840ed10f573
sidiki99 <msidiki075@gmail.com>
2026-02-05 12:29:28 +0500

    useState add and value a array is passed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
  
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -5,12 +7,17 @@ const initialItems = [
 ];
 
 export default  function App(){
+   const [items,setItems]=useState([]);
+
+  function  handleAddItems(item){
+    setItems((items)=>[...items,item]);
+  }
   return(
 
   <div className="app">
    <Logo />
-   <Form />
-   <PackingList />
+   <Form  onAddItems={handleAddItems}/>
+   <PackingList items={items} />
    <Stats />
   </div>
   )
@@ -22,31 +29,45 @@ function Logo() {
   )
 }
 
-function Form() {
+function Form({onAddItems}) {
+  const [description,setDescription]= useState("");
+  const [quantity,setQuantity]= useState(1);
+ 
   function handleSubmit(e){
     e.preventDefault();
+    if(!description) return;
+
+    const newItem ={ description,quantity,packed:false, id:Date.now()}
+  console.log(newItem);
+
+  onAddItems(newItem);
+    
+    setDescription("");
+    setQuantity (1);
   }
+
+  
   return(
    <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your 😍 trip?</h3>
-    <select>
+    <select value={quantity } onChange={(e)=> setQuantity(Number(e.target.value))}>
       {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
         <option value={num} key={num}>
           {num}
         </option>
       ))}
     </select>
-    <input type="text" placeholder=" item..." />
+    <input type="text" placeholder=" item..." value={description} onChange={(e) => setDescription(e.target.value)} />
     <button>Add</button>
 
   </form>)
 }
 
-function PackingList() {
+function PackingList({items}) {
   return (
     <div className="list">
    <ul> 
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item}  />
         ))}
     </ul>    
